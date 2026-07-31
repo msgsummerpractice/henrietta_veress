36cd78751125761a9df8e45c64407ec9cd082bf6
¨heniveress¨ <heniveress850@gmail.com>
2026-07-31 15:45:31 +0300

    Added routing to Login, Home and NotFound pages

--- Angular/my-angular-app/src/app/app.routes.ts
+++ Angular/my-angular-app/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login-component/login-component';
+import { NotFoundComponent } from './not-found-component/not-found-component';
+import { HomeComponent } from './home-component/home-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'notfound', component: NotFoundComponent },
+  { path: '**', redirectTo: 'notfound' },
+];
