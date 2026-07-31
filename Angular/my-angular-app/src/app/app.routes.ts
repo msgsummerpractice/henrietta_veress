4cd0b449a0a131c4af52e20aa5b74e01ef57e07e
¨heniveress¨ <heniveress850@gmail.com>
2026-07-31 17:06:16 +0300

    Added lazy routing and active router link

--- Angular/my-angular-app/src/app/app.routes.ts
+++ Angular/my-angular-app/src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login-component/login-component';
 import { NotFoundComponent } from './not-found-component/not-found-component';
 import { HomeComponent } from './home-component/home-component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
+  {
+    path: 'login',
+    loadComponent: () => import('./login-component/login-component').then((m) => m.LoginComponent),
+  },
   { path: 'notfound', component: NotFoundComponent },
   { path: '**', redirectTo: 'notfound' },
 ];
--- Angular/my-angular-app/src/app/navbar-component/navbar-component.ts
+++ Angular/my-angular-app/src/app/navbar-component/navbar-component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import { RouterLink } from "@angular/router";
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'Navbar',
-  imports: [MatToolbar, MatIconModule, MatButtonModule, MatToolbarModule, RouterLink],
+  imports: [
+    MatToolbar,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './navbar-component.html',
   styleUrl: './navbar-component.css',
 })
