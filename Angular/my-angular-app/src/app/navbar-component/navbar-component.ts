6d40472873d8593e7c5221112fd26c36129a3ed5
¨heniveress¨ <heniveress850@gmail.com>
2026-07-31 15:44:37 +0300

    Minor cleanup: colors, naming, small tweaks

--- Angular/my-angular-app/src/app/navbar-component/navbar-component.ts
+++ Angular/my-angular-app/src/app/navbar-component/navbar-component.ts
@@ -3,11 +3,12 @@ import { MatToolbar } from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { RouterLink } from "@angular/router";
 
 @Component({
-  selector: 'app-navbar',
-  imports: [MatToolbar, MatIconModule, MatButtonModule, MatToolbarModule],
-  templateUrl: './navbar.html',
-  styleUrl: './navbar.css',
+  selector: 'Navbar',
+  imports: [MatToolbar, MatIconModule, MatButtonModule, MatToolbarModule, RouterLink],
+  templateUrl: './navbar-component.html',
+  styleUrl: './navbar-component.css',
 })
-export class Navbar {}
+export class NavbarComponent {}
