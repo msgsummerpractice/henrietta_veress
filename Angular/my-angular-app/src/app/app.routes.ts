af92102e19920c5baa7a9db46701494aa798a4de
¨heniveress¨ <heniveress850@gmail.com>
2026-08-02 22:29:32 +0300

    Added simple route guard

--- Angular/my-angular-app/src/app/app.routes.ts
+++ Angular/my-angular-app/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found-component/not-found-component';
 import { HomeComponent } from './home-component/home-component';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent, canActivate: [authGuard] },
   {
     path: 'login',
     loadComponent: () => import('./login-component/login-component').then((m) => m.LoginComponent),
