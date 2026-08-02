2a1015fa1f19c0dadc9c5c001409ae35f41d98e3
¨heniveress¨ <heniveress850@gmail.com>
2026-08-02 22:20:27 +0300

    Implemented separate dog service

--- Angular/my-angular-app/src/app/home-component/home-component.ts
+++ Angular/my-angular-app/src/app/home-component/home-component.ts
@@ -1,73 +1,25 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { HttpClient } from '@angular/common/http';
 import { WordCountPipe } from '../pipes/word-count-pipe';
-import { App } from "../app";
-
-type DogResponse = {
-  message: string;
-  status: string;
-};
-
-type DogCard = {
-  name: string;
-  description: string;
-  imageUrl: string;
-};
+import { App } from '../app';
+import { DogService } from './dog-service';
 
 @Component({
   selector: 'Home',
   imports: [MatButton, MatToolbarModule, WordCountPipe, App],
   templateUrl: './home-component.html',
   styleUrl: './home-component.css',
+  providers: [DogService],
 })
 export class HomeComponent {
-  dogos = signal<DogCard[]>([
-    {
-      name: 'Labrador',
-      description: 'They are really friendly, great family dog. Intelligent fluffballs.',
-      imageUrl: '',
-    },
-    { name: 'Vizsla', description: 'Beautiful loyal doggo.', imageUrl: '' },
-    { name: 'Akita', description: 'A bit sassy, but extremely loyal to his owner.', imageUrl: '' },
-  ]);
 
-  constructor(private http: HttpClient) {}
+  private readonly dogService = inject(DogService);
 
-  ngOnInit(): void {
-    this.fetchDogPics();
-  }
+  dogos = this.dogService.dogos;
 
-  fetchDogPics(): void {
-    this.http.get<DogResponse>('https://dog.ceo/api/breed/labrador/images/random').subscribe({
-      next: (response) => {
-        const dogs = this.dogos();
-        dogs[0].imageUrl = response.message;
-        this.dogos.set([...dogs]);
-      },
-      error: (err) => console.error('Labrador fetch failed:', err),
-    });
-
-    this.http.get<DogResponse>('https://dog.ceo/api/breed/vizsla/images/random').subscribe({
-      next: (response) => {
-        const dogs = this.dogos();
-        dogs[1].imageUrl = response.message;
-        this.dogos.set([...dogs]);
-      },
-      error: (err) => console.error('Vizsla fetch failed:', err),
-    });
-
-    this.http.get<DogResponse>('https://dog.ceo/api/breed/akita/images/random').subscribe({
-      next: (response) => {
-        const dogs = this.dogos();
-        dogs[2].imageUrl = response.message;
-        this.dogos.set([...dogs]);
-      },
-      error: (err) => console.error('Akita fetch failed:', err),
-    });
-  }
   onButtonClick(): void {
-    this.fetchDogPics();
+    this.dogService.fetchDogPics();
   }
 }
