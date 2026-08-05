820b6eeace6da51ae70a1fcd131a3639809e734a
¨heniveress¨ <heniveress850@gmail.com>
2026-08-06 00:04:44 +0300

    Corrected CORS error

--- Java/spring-security/src/main/java/com/example/spring_rest/config/SecurityConfig.java
+++ Java/spring-security/src/main/java/com/example/spring_rest/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(csrf -> csrf.disable())
+        http.cors(cors -> {}).csrf(csrf -> csrf.disable())
 
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
